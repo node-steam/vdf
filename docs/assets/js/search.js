2ed1916311c633fa79cb9f27cc66a69bc484e659
Silas Rech <silas@panascais.net>
2021-07-02 23:10:38 +0200

    Update docs

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -1,3 +1,1 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"parse","url":"globals.html#parse","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"stringify","url":"globals.html#stringify","classes":"tsd-kind-function"}]};
+window.searchData = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"parse","url":"modules.html#parse","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"stringify","url":"modules.html#stringify","classes":"tsd-kind-function"}],"index":{"version":"2.3.9","fields":["name","parent"],"fieldVectors":[["name/0",[0,6.931]],["parent/0",[]],["name/1",[1,6.931]],["parent/1",[]]],"invertedIndex":[["parse",{"_index":0,"name":{"0":{}},"parent":{}}],["stringify",{"_index":1,"name":{"1":{}},"parent":{}}]],"pipeline":[]}}
